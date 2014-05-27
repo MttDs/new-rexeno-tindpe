edf3a777752521b0c3b76f67c4f0efa71019a99a
Matthias-Kelvin Daous <mk.daous@gmail.com>
2014-05-27 12:22:15 +0200

    update default protocole

--- protocoles/default/main.cc
+++ protocoles/default/main.cc
@@ -10,7 +10,7 @@ using namespace std;
 
 void		GO_Target1(VariableManager& TM)
 {
-  Variable	*fixation_duration = TM.getVariable("Fixation_Duration");
+ /* Variable	*fixation_duration = TM.getVariable("Fixation_Duration");
   // Variable	*fixation_duration = new Variable(0);
   Variable	*end_fixation = TM.getVariable("End_Fixation");
   Variable	*end_target = TM.getVariable("End_Target");
@@ -19,12 +19,12 @@ void		GO_Target1(VariableManager& TM)
   fixation_duration->value = rand() % 30 + 30; // Un nombre random entre 30 et 60
   //end_fixation->value = fixation_duration->value;
   end_fixation->value = 1000;
-  end_target->value = end_fixation->value + 60;
+  end_target->value = end_fixation->value + 60;*/
 }
 
 void		GO_Target2(VariableManager& TM)
 {
-  Variable	*fixation_duration = TM.getVariable("Fixation_Duration");
+ /* Variable	*fixation_duration = TM.getVariable("Fixation_Duration");
   // Variable	*fixation_duration = new Variable(0);
   Variable	*end_fixation = TM.getVariable("End_Fixation");
   Variable	*end_target = TM.getVariable("End_Target");
@@ -33,7 +33,7 @@ void		GO_Target2(VariableManager& TM)
   fixation_duration->value = rand() % 30 + 30; // Un nombre random entre 30 et 60
   //end_fixation->value = fixation_duration->value;
   end_fixation->value = 1000;
-  end_target->value = end_fixation->value + 60;
+  end_target->value = end_fixation->value + 60;*/
 }
 
 
@@ -41,7 +41,7 @@ void		InterTrial_CTM(std::string &name,
                                VariableManager& TM)
 			       //int r) // Sera executée à chaque intertrial !
 {
-  //std::cout << "REDO ? " << r << std::endl;
+ /* //std::cout << "REDO ? " << r << std::endl;
   // if (r == 1)
   //   {
   //     std::cout << "Send Pulse" << std::endl;
@@ -52,7 +52,7 @@ void		InterTrial_CTM(std::string &name,
   //Variable* start = TM.getVariable("Start_Correct");
   Variable* end = TM.getVariable("End_Target");
   //start->value = 1160;
-  end->value = 1160;
+  end->value = 1160;*/
 }
 
 
@@ -64,7 +64,9 @@ int	main(int argc,
   configuration::SessionInfo conf;
   //bool r = configuration::CreateConfiguration("../../protocoles/DevTests/conf", conf);
   bool r = configuration::CreateConfiguration("definition", conf);
-  Order o("order");
+  Order o("order", conf);
+  o.parse();
+
   //assert(r);
   Setup* setup = new Setup("~/.rexeno");
   Session* session = Session::getInstance(conf, o);
